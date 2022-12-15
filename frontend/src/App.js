87fd64aa7300cc0db84121048e4eeed1b76fee74
FranAlcoba66 <falcoba@picallex.com>
2022-12-15 11:16:50 -0300

    fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import HomeComponent from './components/HomeComponent';
+
+
+
 import NavbarComponent from './components/NavbarComponent';
 function App() {
   return (
     <>
     <NavbarComponent></NavbarComponent>
-   <HomeComponent></HomeComponent>
+    <HomeComponent></HomeComponent>
     </>
   );
 }
